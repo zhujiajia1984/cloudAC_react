a848fe4f5208bdc07469cee6716b7a59d7b48136
zhujiajia <772978409@qq.com>
2018-03-29 17:00:01 +0800

    login页面样式完成

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import LoginMain from '../../Components/LoginMain/LoginMain';
+import LoginForm from '../../Components/LoginForm/LoginForm';
+import { Tabs } from 'antd';
 import './Login.less';
-import LoginMain from '../../Components/LoginMain/LoginMain'
+
+//
+const TabPane = Tabs.TabPane;
 
 //
 export default class Login extends React.Component {
@@ -11,7 +16,16 @@ export default class Login extends React.Component {
         return (
             <div>
 				<LoginMain>
-                    <span>abc</span>            
+                    <Tabs defaultActiveKey="account" className="loginTabStyle">
+                        <TabPane tab="账号登录" key="account">
+                            <LoginForm></LoginForm>
+                            <div className="loginFooterStyle">
+                                CloudAC ©2017 Created by DoubleCom
+                            </div>
+                        </TabPane>
+                        <TabPane tab="微信登录" key="wx" className="qrCodeWrapper">
+                        </TabPane>
+                    </Tabs>
                 </LoginMain>
             </div>
         );
